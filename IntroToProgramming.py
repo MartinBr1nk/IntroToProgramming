6663afa2888823dd0a68c47cc3633afa2ebefada
MartinBr1nk <martin.brink@bcestudent.com>
2024-09-12 14:11:53 +0100

    Changed the inital code
    Changed the initial code to start making the actual program

--- IntroToProgramming.py
+++ IntroToProgramming.py
@@ -1,2 +1,8 @@
-print("hello world")
-#spingus bingus
+total = 0
+print("This Is Journey to the Path of Glory.")
+nameplease= input("Young Traveller what is your name ")
+confirm = input("you have chosen the name", nameplease,"is this correct?")
+if confirm == "yes":
+    print("you have chosen the name of", nameplease)
+elif nameplease == "no":
+    nameplease = input("please reenter your name")
