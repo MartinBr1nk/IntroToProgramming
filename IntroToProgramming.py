e35fba64ea7f816b841168d395da2d44a52915dd
MartinBr1nk <martin.brink@bcestudent.com>
2024-09-18 12:08:21 +0100

    Added respirating, Masterplan and More!
    Farming yet to come

--- IntroToProgramming.py
+++ IntroToProgramming.py
@@ -4,16 +4,8 @@ floor = 8
 lockdownprotocol = False
 cropamount = 0
 
-#Achievements!
-singitloud = False
-hungyforoxygen = False
-finishedthegame= False
-didthebrief = False
-yougotacoolname = False
 
-
-
-import time, random
+import time, random,achivements, themasterplan
 def print_slow(str):
     for letter in str:
         print(letter, end = "")
@@ -25,109 +17,12 @@ def print_faster(str):
         time.sleep(random.uniform(0.001, 0.0005))
 
 
+lockallthedoorsmaybewellneverfindit = themasterplan.lockdownlogo
+singitloud = themasterplan.masterplan
+terminalbootup  = achivements.doster
 
-   
-print_faster(r""" 
-┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                       '||''|.    ..|''||    .|'''.|  |''||''|                                                │
-│                                        ||   ||  .|'    ||   ||..  '     ||      ....  ... ..                                 │
-│                                        ||    || ||      ||   ''|||.     ||    .|...||  ||' ''                                │
-│                                        ||    || '|.     || .     '||    ||    ||       ||                                    │
-│                                       .||...|'   ''|...|'  |'....|'    .||.    '|...' .||.                                   │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │
-│                                                                                                                              │ 
-│                                                                                                                              │
-│                                                The DosTerminal Corporation © 1963                                            │
-└──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+terminalbootup
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""")
 
 print("Terminal Assistant -Last Updated- Jan 24th 1963")
 nameplease= input("Input Agent Name ").upper()
@@ -143,67 +38,48 @@ elif incomingmessage == "N":
         print("You hear a noise coming from above you. Curious you leave the secure room and investigate, An Intruder then with a machete beheads you and it is game over.")
         life = 0
 while life == 1:       
-        print_slow(" Please Do the following in any order: 1. Lock All The Doors 2. Harvest Crops 3. Initiate Oxygen Deprivation Protocol 4. Lock The Armoury 5. Avoid taxes 6. Check Camera 7. Play a Game! ")
+        print_slow("\n Please Do the following in any order: 1. Lock All The Doors 2. Harvest Crops 3. Initiate Oxygen Deprivation Protocol 4. Lock The Armoury 5. Avoid taxes 6. Check Camera 7. Play a Game! ")
         whichone = input ("what will you do first? enter any number")
         if whichone == "1":
             pickadoor = input("which door do you check? Door One: Past the Corridor, Door Two: Infront of you, Door Three: Down to The 9th Floor")
-            if pickadoor == "3":
+            if pickadoor == "3": #you go down a floor and then turn on a computer
                 print("you have descended doors down to Floor 9. You started on the 8th Floor and now you have descended down the steps and into the new floor with 4 rooms. You check the room assigned in the terminal and look into it, There is nothing but a New Terminal with Red Tape Keeping the Screen in Place. There is a Sticky Note on the Desk")
                 floor = floor + 1
                 readnote = input("do you read the note?")
                 if readnote == "yes":
                     print("The note says - This Terminal Has Passed Health And Safety tests")
+                    print_faster(r"""   ______________
+                                       /             /|
+                                      /             / |
+                                     /____________ /  |
+                                    | ___________ |   |
+                                    ||           ||   |
+                                    ||  BOOT UP  ||   |
+                                    || INITIATED ||   |
+                                    ||___________||   |
+                                    |   _______   |  /
+                                   /|  (_______)  | /
+                                  ( |_____________|/
+                                   \
+                                .=======================.
+                                | ::::::::::::::::  ::: |
+                                | ::::::::::::::[]  ::: |
+                                |   -----------     ::: |
+                                `-----------------------'""")
                     logonterminal = input("will you log onto the terminal?")
                     if logonterminal == "Y":
                         print_slow("Hello", nameplease, "Welcome Back to the terminal. What Would You Like to Do?")
                         print(pickadoor)
-        elif pickadoor == "1": #Creator Decided to Put the masterplan by oasis in as the song you sing 
-            print("Down the corridor you go! as you keep going down the corridor you contemplate whether you sing a little song or not.")
-            sing = input("do you sing?")
-            if sing =="yes":
-                print("so you have decided to sing! so you sing the masterplan by oasis and you say:")
-                print_slow(r""" \n 
-                          Take the time to make some sense
-                          Of what you want to say
-                          And cast your words away upon the waves
-                          Sail them home with acquiesce
-                          On a ship of hope today
-                          And as they land upon the shore
-                          Tell them not to fear no more
-                          Say it loud, and sing it proud today
-                          And then
-                          Dance if you wanna dance
-                          Please, brother, take a chance
-                          You know they're gonna go
-                          Which way they wanna go
-                          All we know is that we don't know
-                          How it's gonna be
-                          Please, brother, let it be
-                          Life on the other hand
-                          Won't make us understand
-                          We're all part of a master plan
-                          Say it loud, and sing it proud today
-                          I'm not saying right is wrong
-                          It's up to us to make
-                          The best of all the things that come our way
-                          'Cause everything that's been has passed
-                          The answer's in the looking glass
-                          There's four and twenty million doors
-                          On life's endless corridor
-                          Say it loud, and sing it proud
-                          And they
-                          Will dance if they wanna dance
-                          Please, brother, take a chance
-                          You know they're gonna go
-                          Which way they wanna go
-                          All we know is that we don't know
-                          How it's gonna be
-                          Please, brother, let it be
-                          Life on the other hand
-                          Won't make you understand
-                          We're all part of a master plan""")
+            elif pickadoor == "1": #Creator Decided to Put the masterplan by oasis in as the song you sing 
+                print("Down the corridor you go! as you keep going down the corridor you contemplate whether you sing a little song or not.")
+                sing = input("do you sing?")
+                if sing =="yes":
+                    print("so you have decided to sing! so you sing the masterplan by oasis and you say:")
+                    print_slow(singitloud)
             elif sing =="no":
                 print("well. you are no fun.")
+                print("mr boring man continues to walk down until he reaches the corridor and then see")
+                youdidntsing = True
         
         if whichone == "2": #farming! coming probably not soon
             print("you go to the open space which has lights on 24/7 to simulate daytime, there are different crops available to harvest: Corn, Potato, Carrot, Lettuce, Cabbage, Tomato, Pumpkin, Watermelon, Onion and Strawberry ")
@@ -220,21 +96,11 @@ while life == 1:
                     print("total amount of crops you have harvested is", cropamount)
                     harvestmoon = input("keep harvesting or press 0 to stop")
 
-        if whichone == "3":
+        if whichone == "3": #lockdown protocol which so far doesnt do anything
             lockthatdown=input("Please Enter your Name to Initiate Lockdown Protocol").upper()
             if lockthatdown == nameplease:
                 print("Oxygen Deprivation Protocol has Started.")
                 lockdownprotocol = True
-                print_faster(r"""  
-                / \               / \
-               /   \             /   \
-              (_____)           (_____)
-               |   |  _   _   _  |   |
-               | O |_| |_| |_| |_| O |
-               |-  |          _  | - |
-               |   |   - _^_     |   |
-               |  _|    //|\\  - |   |
-               |   |   ///|\\\   |  -|
-               |-  |_  |||||||   |   |
-               |   |   |||||||   |-  |
-               |___|___|||||||___|___|""")
+                print_faster(lockallthedoorsmaybewellneverfindit)
+
+            lifeordeath = input("do you put on your respirator? it is a small device that will allow you to continue to be able to breathe. without it you could pass out ")
--- achivements.py
+++ achivements.py
@@ -1,7 +1,186 @@
-#Achievements!
+﻿#Achievements!
 singitloud = False
 hungyforoxygen = False
 finishedthegame= False
 didthebrief = False
 yougotacoolname = False
+youdidntsing = False
+
+import time, random
+def print_slow(str):
+    for letter in str:
+        print(letter, end = "")
+        time.sleep(random.uniform(0.01, 0.05))
+
+def print_faster(str):
+    for letter in str:
+        print(letter, end = "")
+        time.sleep(random.uniform(0.001, 0.0005))
+
+doster = print_faster(r""" 
+┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                       '||''|.    ..|''||    .|'''.|  |''||''|                                                │
+│                                        ||   ||  .|'    ||   ||..  '     ||      ....  ... ..                                 │
+│                                        ||    || ||      ||   ''|||.     ||    .|...||  ||' ''                                │
+│                                        ||    || '|.     || .     '||    ||    ||       ||                                    │
+│                                       .||...|'   ''|...|'  |'....|'    .||.    '|...' .||.                                   │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │
+│                                                                                                                              │ 
+│                                                                                                                              │
+│                                                The DosTerminal Corporation © 1963                                            │
+└──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                     .;+itIYIIYIIYItt+;:                      
+                  =tXVVXItt+;=;;=+itIYVVXIi:                  
+               ;YVXXt=:               .;tYXXX+                
+            .tVXVt;                       .iXXVI:             
+           tRXX;                        .;;  ;YVVY:           
+         ;RXV;                            +RR+..YVRt          
+       .XXR;                           :;.  ;BBY.:VXR;        
+      ;RYX                              :XBY; tBBi tVR+       
+     ;RV+                             :;  :RMB+;BMB.;RXt      
+    ;RR;                               ;RR+.tBMBiBMB::RXt     
+   :RV;                             .+;. iBBY;BMMBMMB.:RXi    
+   RYt                                =BBi:BMBXBMMMBY. ;RR;   
+  YtV                               +Y+:tMBIBMMBMR=     iIR   
+ :RR.                        .::.    .tBXtMMBMBY:        RIi  
+ XIt                     ;IRBRXVRBY;   ;BBBMR=           ;VR  
+.RR                    ;RBt:     .+RB;  :BI:              VY; 
++tX                   iBt           =BX                   itY 
+Yt=                  ;Bi             :Bt                  :XR 
+RX:                  RX               =B:                  RX.
+RR                  :B:                Ri                  YX:
+RR                  ;B                 YY                  YY;
+RR                  :B;                Ri                  YX:
+RX:                  RX               +B:                  RX.
+YI;                  +Ri             :Bt                  :XR 
++tX              .i:  iBI           iBX                   itY 
+.RR            ;VBBB.  ;RBt:     :+RR+                    VY; 
+ XIt        .tBMMMRBB;   ;tRBVXVBRY;                     ;VR  
+ :RR.     ;RBMMBVBB;;RR;     .::.                        RIi  
+  IIR  .tBMMMBMMB;RMX:                                  tIR   
+   RII BMMBMMBiBMB;:+Vi:                               ;RR:   
+   :RV;:BMBXBMB;;VBY.                                 :RX+    
+    ;RV+:BMB;RBMY  ;t;                               ;RXt     
+     ;RXt.RMB;:tBB=                                 ;RVi      
+      :RYR.;BBY. :+t;                              IXR;       
+        XXR+ ;RBY:                               ;RYR:        
+         ;RYR; .;Yi:                           :XXVt          
+           +VXV=                             ;YVVt.           
+             +VXVI;                       ;tVXVt.             
+               ;tVXXYi;.              ;+IXXVY;                
+                  ;tIVVXXYti=====iiIXXVVYt;                   
+                      :;iitItttttItt+;:
+
+
+
+
+LOADING DISK DRIVE
+64MB RAM                                                CHECK
+DISK  DRIVE                                             OPERATIONAL
+CPU                                                     CHECK
+FLOPPY DISK                                             ! Error !
+ADVANCED GRAPHICS                                       ! Error !
+KEYBOARD                                                PRESENT
+MOUSE                                                   ! Error !
+
+
+
+
+
+
+
+
+
+
+,---,---,---,---,---,---,---,---,---,---,---,---,---,-------,
+| ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | [ | ] | <-    |
+|---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|
+| ->| | " | , | . | P | Y | F | G | C | R | L | / | = |  \  |
+|-----',--',--',--',--',--',--',--',--',--',--',--',--'-----|
+| Caps | A | O | E | U | I | D | H | T | N | S | - |  Enter |
+|------'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'--------|
+|        | ; | Q | J | K | X | B | M | W | V | Z |          |
+|------,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|
+| ctrl |  | alt |                          | alt  |  | ctrl |
+'------'  '-----'--------------------------'------'  '------'
+
+YOUR KEYBOARD IS THE MOST IMPORTANT THING. DO NOT LET ANYONE TAMPER WITH THE CONSOLE OR THE KEYBOARD. ANY MISSING KEYS WILL COME OUT OF YOUR PAYCHECK IF DISCOVERED BY A SPONTANEOUS INSPECTION BY YOUR SUPERIORS.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""")
+
+
 
